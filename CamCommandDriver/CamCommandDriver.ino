9ca97fe6e5ad5ac686960de376d8146909b0fa8b
Austenh <austenh@qti.qualcomm.com>
2014-07-25 09:14:57 -0400

    More elegant parser and connection manager

--- CamCommandDriver/CamCommandDriver.ino
+++ CamCommandDriver/CamCommandDriver.ino
@@ -19,13 +19,6 @@
 #define DEFAULT_ADJUST 4
 #define BIG_ADJUST 32
 
-#include <Wire.h>
-//Requires Adafruit servo driver
-#include <Adafruit_PWMServoDriver.h>
-
-// called this way, it uses the default address 0x40
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
 //Expect Servos in slot 1 and slot 2
 #define TILT_SERVO 1
 #define PAN_SERVO 0
@@ -33,10 +26,37 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define TILT_CENTER 70
 #define PAN_CENTER 127
 
+#define DISCARD_INPUT_STATE 0
+#define TIME_READ_STATE 1
+#define COMMAND_READ_STATE 2
+
+
+#include <Wire.h>
+//Requires Adafruit servo driver
+#include <Adafruit_PWMServoDriver.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x33, 0x49 };
+char server[] = "camcommand.herokuapp.com";    
+IPAddress ip(192,168,0,18);
+EthernetClient client;
+
+
 uint8_t user_tilt = 70;
 uint8_t user_pan = 127;
 
 char serialCmd;
+
+char time_buffer[256];
+char command_buffer;
+
+uint8_t time_ptr = 0;
+uint32_t current_time = 0;
+boolean last_connected = false;
 
 int setTilt(uint8_t tilt)
 {
@@ -61,8 +81,6 @@ int setPan(uint8_t pan)
   //setServoPulse(PAN_SERVO, pulse);
   pwm.setPWM(PAN_SERVO, 0, pulse);
 }
-
-
 
 void adjust(char byteRecieved, uint8_t adjustment = DEFAULT_ADJUST)
 {
@@ -96,55 +114,129 @@ void adjust(char byteRecieved, uint8_t adjustment = DEFAULT_ADJUST)
   setTilt(user_tilt);
 }
 
-
-//Parse the web server
-#include <SPI.h>
-#include <Ethernet.h>
-
-
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x33, 0x49 };
-char server[] = "camcommand.herokuapp.com";    
-IPAddress ip(192,168,0,18);
-EthernetClient client;
-
-
-char buffer[256];
-uint8_t buffer_ptr = 0;
-uint8_t command_ptr = 0;
-uint32_t current_time = 0;
-boolean command_valid = false;
-boolean last_connected = false;
-
-void queryCommands()
+void HTTP(char *request)
 {
   if (client.connect(server, 80)) {
 #ifdef DEBUG
     Serial.println("Sending Query");
 #endif
-    // Make a HTTP request:
-    sprintf(buffer, "GET /commands/list.txt?timestamp=%u HTTP/1.1", current_time);
-    client.println(buffer);
+    client.println(request);
     client.println("Host: camcommand.herokuapp.com");
+    client.println("User-agent: camcommand-client-arduino");
     client.println("Connection: close");
     client.println();
-  } 
+  }else{
+#ifdef DEBUG
+    Serial.println("Connection failed");
+#endif
+    client.stop();
+  }
 }
 
-void sendClear()
+void queryCommands()
 {
-  if (client.connect(server, 80)) {
-#ifdef DEBUG
-    Serial.println("Sending Clear");
-#endif
-    // Make a HTTP request:
-    client.println( "GET /commands/clear?confirm=yes HTTP/1.1");
-    client.println("Host: camcommand.herokuapp.com");
-    client.println("Connection: close");
-    client.println();
+  char buffer[256];
+  // Make a HTTP request:
+  sprintf(buffer, "GET /commands/list.txt?timestamp=%u HTTP/1.1", current_time);
+  HTTP(buffer); 
+}
+
+void clearCommands()
+{
+  HTTP("GET /commands/clear?confirm=yes HTTP/1.1");
+  if(client.connected())
+  {
     client.flush();
     client.stop();
+  }
+}
+
+boolean connectionReady()
+{
+  boolean ready = client.connected();
+  if (!ready && last_connected) {
+#ifdef DEBUG
+    Serial.println("Disconnecting.");
+#endif
+    client.stop();
+  }
+  last_connected = ready;
+  return ready;
+}
+
+boolean isDigit(char data)
+{
+  return (data >= '0') && (data <= '9');
+}
+
+void parseCommandStream()
+{
+  //FSM model
+  char data = 0;
+  uint8_t state = DISCARD_INPUT_STATE;
+
+  while(client.available() > 0)
+  {
+    data = client.read();
+    if(data == -1)
+      break;
+    switch(state)
+    {
+      case DISCARD_INPUT_STATE:
+        if(!isDigit(data))
+        {
+#ifdef DEBUG
+          Serial.write(data);
+          delay(10);
+#endif
+          break;
+        }else{
+          time_ptr = 0;
+          state = TIME_READ_STATE;
+#ifdef DEBUG
+          Serial.println("Reading Time");
+#endif
+        } //INTENTIONAL FALLTHROUGH
+
+      case TIME_READ_STATE:
+        if(data != ':')
+        {
+#ifdef DEBUG
+          Serial.write(data);
+          delay(10);
+#endif
+          if(isDigit(data))
+          {
+            time_buffer[time_ptr++] = data;
+          }else{
+#ifdef DEBUG
+            Serial.println("Invalid character");
+#endif
+            state = DISCARD_INPUT_STATE;
+          }
+        }else{
+          //Intentionally discard data
+          time_buffer[time_ptr] = '\0';
+          current_time = atoi(time_buffer);
+          state = COMMAND_READ_STATE;
+        }
+        break;
+
+      case COMMAND_READ_STATE:
+        //We only need one character, just take the first one and discard the rest of the line
+#ifdef DEBUG
+            Serial.println("Command Accepted");
+            Serial.println(data);
+#endif
+        command = data;
+        //Execute command
+        adjust(command);
+        state = DISCARD_INPUT_STATE;
+        break;
+
+      default:
+        break;
+    }
   }
 }
 
@@ -181,39 +273,12 @@ void loop()
     adjust(serialCmd);
   }
 #endif
-    if (client.available()) {
-      buffer[buffer_ptr] = client.read();
-      if(buffer[buffer_ptr] == '\n')
-      {
-        if(command_valid)
-        {
-          adjust(buffer[command_ptr]);
-        }
-        buffer_ptr = 0;
-        command_ptr = 0;
-        command_valid = false;
-      }else if(buffer[buffer_ptr] == ':'){
-
-        command_valid = true;
-        buffer[buffer_ptr] = '\0';
-        command_ptr = buffer_ptr++;
-
-      }else{
-		++buffer_ptr;      
-	  }
-    }
-    // if the server's disconnected, stop the client:
-  if (!client.connected() && last_connected) {
-    last_connected = false;
-    sendClear();
-    //Reset
-    buffer_ptr = 0;
-    command_ptr = 0;
-    command_valid = false;   
+  if(connectionReady())
+  {
+    parseCommandStream();
+    clearCommands(); //Accumulate commands  
     delay(1000);
-    queryCommands();
-
   }else{
-     last_connected = client.connected();
+    queryCommands();
   }
 }
